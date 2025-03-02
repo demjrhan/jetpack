499ae513b285b2a1f44b8af4eb68a9a34f0d77fb
demjrhan <demirhanylcn@gmail.com>
2025-03-02 15:05:05 +0100

    Combined HorizontalPager knowledge with Card knowledge to create sort of main page ui. Future goal; adding youtube short sized horizontalPager part for bottom part of screen

---
+++ Youtube_Home/app/src/main/java/test/youtube_home/MainActivity.kt
@@ -0,0 +1,151 @@
+package test.youtube_home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import test.youtube_home.ui.theme.Youtube_HomeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Youtube_HomeTheme {
+
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HomePage(list: List<Int>, description: String) {
+    val thumbnailBrush = Brush.linearGradient(
+        listOf(
+            Color(0xFFFADF70), // Soft Yellow
+            Color(0xFFEC635E), // Coral Red
+            Color(0xFF7AC07B), // Soft Green
+            Color(0xFF437BDA), // Blue
+            Color(0xFF000000)  // Black
+        )
+    )
+    val screenBrush = Brush.verticalGradient(
+        listOf(
+            Color(0xFF000000), // Pure Black
+            Color(0xFF141414), // Deep Charcoal
+            Color(0xFF282828) // Dark Graphite
+        )
+
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(screenBrush, RoundedCornerShape(1.dp))
+    ) {
+        repeat(3) { index ->
+            if (index < list.size) {
+
+
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                ) {
+                    HorizontalPager(
+                        state = rememberPagerState(
+                            initialPage = 0,
+                            pageCount = { list.size }),
+                        pageSpacing = 16.dp
+                    ) {
+                        Card(
+                            colors = CardDefaults.cardColors(Color.White)
+                        ) {
+
+                            Image(
+                                modifier = Modifier
+                                    .background(thumbnailBrush, RoundedCornerShape(1.dp))
+                                    .weight(0.7f)
+                                    .fillMaxSize()
+                                    .padding(12.dp)
+                                    .clip(
+                                        RoundedCornerShape(
+                                            topStart = 15.dp,
+                                            topEnd = 15.dp,
+                                            bottomStart = 8.dp,
+                                            bottomEnd = 8.dp
+                                        )
+                                    ),
+                                contentScale = ContentScale.Crop,
+                                painter = painterResource(list[index]),
+                                contentDescription = description
+                            )
+                            Box(modifier = Modifier.weight(0.3f)) {
+                                Column(modifier = Modifier.fillMaxSize(), Arrangement.Center) {
+                                    Text(
+                                        text = "Title",
+                                        modifier = Modifier
+                                            .padding(start = 16.dp),
+                                        fontWeight = FontWeight.SemiBold,
+                                        fontFamily = FontFamily.Monospace,
+                                        fontSize = 18.sp
+                                    )
+                                    Text(
+                                        text = "Channel Name",
+                                        modifier = Modifier
+                                            .padding(start = 16.dp),
+                                        fontWeight = FontWeight.Normal,
+                                        fontFamily = FontFamily.Monospace,
+                                        fontSize = 13.sp
+                                    )
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    Youtube_HomeTheme {
+        val list = listOf(
+            R.drawable.feel_like_yourself, R.drawable.logo, R.drawable.feel_like_yourself
+        )
+        HomePage(list = list, description = "empty description")
+    }
+}
